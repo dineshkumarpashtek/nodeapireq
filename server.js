99d8149ded8f2e55d57ca455410d0bb3097cc822
Vignesh Pashtek <vignesh.pashtek@gmail.com>
2017-12-03 23:28:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
-//var mongoose = require('mongoose');
-var passport = require('passport');
 var flash    = require('connect-flash');
 
 var morgan       = require('morgan');
@@ -13,9 +11,7 @@ var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
 var pgp = require('./server/config/pgpromise.js');
-var db= require('./server/config/database.js') // load our db
-require('./server/config/passport')(passport,db,pgp); // pass passport for configuration
-
+var db= require('./server/config/database.js') 
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -24,15 +20,11 @@ app.use(bodyParser()); // get information from html forms
 app.set('views', './server/views');
 app.set('view engine', 'ejs'); // set up ejs for templating
 
-// required for passport
-app.use(session({ secret: 'ramkosalramkosal' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+
 app.use(express.static("./public"));
 
 // routes ======================================================================
-require('./server/config/routes.js')(app, passport,db,pgp); // load our routes and pass in our app and fully configured passport
+require('./server/config/routes.js')(app,db,pgp); // load our routes and pass in our app and fully configured passport
 
 
 // launch ======================================================================
