b3301b0ab45cb33601217f14cb006ebd142a404c
vigneshEpisource <49470280+vigneshEpisource@users.noreply.github.com>
2020-04-21 14:35:25 +0530

    Update routes.js
    xx

--- server/config/routes.js
+++ server/config/routes.js
@@ -23,77 +23,17 @@ module.exports = function (app, db, pgp) {
   app.get("/api/createlead/:sId", function (req, res) {
     debugger;
     var sId = req.params.sId;
-    const { author, title } = req.body;
+    const { name, company } = req.body;
 
     console.log("sId+" + sId);
-    console.log("author+" + author + ": title :" + title);
+    console.log("name+" + name + ": company :" + company);
     //WHERE s_id ="+sId+"::int"
-    db.query("SELECT * FROM lead where id=" + sId + "", true)
+    db.query(
+      "INSERT INTO student(name, company)VALUES('vicky1', 'company')",
+      true
+    )
       .then(function (data) {
         console.log("data+" + data);
-        var studentDtls = data;
-
-        conn.login(process.env.SF_Username, process.env.SF_PWD, function (
-          err,
-          userInfo
-        ) {
-          if (err) {
-            return res.redirect("/orders");
-          }
-          // Now you can get the access token and instance URL information.
-          var records = [];
-          conn.query(
-            "SELECT LastName,Company,Id,Name FROM lead WHERE Id='" +
-              sId +
-              "' LIMIT 1",
-            function (err, result) {
-              if (err) {
-                return res.status(500).json({ success: false, error: err });
-              }
-              console.log("total : " + result.totalSize);
-              console.log("fetched : ", result.records[0]);
-              console.log("done ? : " + result.done);
-
-              records = result.records[0];
-
-              if (result.totalSize != 0) {
-                // record updation
-                conn.sobject("lead").update(
-                  {
-                    Id: result.records[0].Id,
-                    Name: result.records[0].name,
-                    LastName: result.records[0].LastName,
-                    Company: result.records[0].Company,
-                  },
-                  function (err, ret) {
-                    if (err || !ret.success) {
-                      return console.error(err, ret);
-                    }
-                    console.log("record Updated successfully : " + ret.id);
-                    // ...
-                  }
-                );
-              } else {
-                // Single record creation
-                conn.sobject("lead").create(
-                  {
-                    Name: studentDtls[0].name,
-                    LastName: studentDtls[0].LastName,
-                    Company: studentDtls[0].Company,
-                  },
-                  function (err, ret) {
-                    if (err || !ret.success) {
-                      return console.error(err, ret);
-                    }
-                    console.log("Created record id : " + ret.id);
-                    // ...
-                  }
-                );
-              }
-            }
-          );
-        });
-
         return res.json(data);
       })
       .catch(function (err) {
