29a3392b7f831df603c987b7683d1b40d93f5ced
vigneshEpisource <49470280+vigneshEpisource@users.noreply.github.com>
2020-04-21 13:39:03 +0530

    CC

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,15 @@
- 
-var pgp = require('./pgpromise.js');
+var pgp = require("./pgpromise.js");
 // See also: https://github.com/vitaly-t/pg-promise#initialization-options
-  
+
 // Database connection details;
-var cn = {  
-    host: 'ec2-54-210-128-153.compute-1.amazonaws.com', // 'localhost' is the default;
-    port: 5432, // 5432 is the default;
-    database: 'd19kndcbml5l6l',
-    user: 'fmudtchuudsjqi',
-    password: 'f62a8692a99f0f2949aec22ad0c9384220b7eab4debd4bc776e83a19d911f06d'
-};        
+var cn = {
+  host: "ec2-52-71-85-210.compute-1.amazonaws.com", // 'localhost' is the default;
+  port: 5432, // 5432 is the default;
+  database: "de69rdrn5us4re",
+  user: "fwgfuwzxriesph",
+  password: "8ef513949750927071e5fc26b91accafd51bfe6e45f293109c2988850f6b7bab",
+};
 // You can check for all default values in:
 // https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
 var db = pgp(cn); // database instance;
-module.exports = db;   
+module.exports = db;
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,111 +1,141 @@
-var jsforce = require('jsforce');
+var jsforce = require("jsforce");
 
 // Salesforce OAuth2 client information
 var conn = new jsforce.Connection({
-  oauth2 : {
+  oauth2: {
     // you can change loginUrl to connect to sandbox or prerelease env.
-    loginUrl : 'https://login.salesforce.com',
+    loginUrl: "https://login.salesforce.com",
     clientId: process.env.Consumer_Key,
-    clientSecret:  process.env.Consumer_Secret,
+    clientSecret: process.env.Consumer_Secret,
     redirectUri: process.env.Callback_URL,
-  }
+  },
 });
 
 //all the routes for our application
-module.exports = function(app,db,pgp) {
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
-    app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
-    });
-         
-   	app.get('/api/studentid/:sId', function(req, res) {
-		      
-        var sId = req.params.sId;   
-        console.log('sId+'+sId);      
-                  //WHERE s_id ="+sId+"::int"
-		db.query("SELECT * FROM lead where s_id="+sId+"", true)
-	    .then(function (data) {
-		console.log('data+'+data);
-		var studentDtls = data;  
+module.exports = function (app, db, pgp) {
+  // =====================================
+  // HOME PAGE (with login links) ========
+  // =====================================
 
-		conn.login(process.env.SF_Username, process.env.SF_PWD, function(err, userInfo) {
-			  if (err) {  return res.redirect('/orders'); }
-			  // Now you can get the access token and instance URL information.
-			var records = [];  
-			conn.query("SELECT LastName,Company,Id,Name FROM lead WHERE Id='"+sId+"' LIMIT 1", function(err, result) {
+  const postLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 1,
+  });
 
-				if (err) { return res.status(500).json({ success: false,error : err}); }
-				console.log("total : " + result.totalSize);
-				console.log("fetched : " , result.records[0]);
-				console.log("done ? : "+ result.done);
-         
-				records = result.records[0];
-    			      
-				if (result.totalSize != 0) {      
-					// record updation
-					conn.sobject("lead").update({   
-						Id : result.records[0].Id,
-						Name : result.records[0].name,     
-						LastName: result.records[0].LastName,
-						Company: result.records[0].Company
-						}, function(err, ret) {
-						  if (err || !ret.success) { return console.error(err, ret); }
-						     console.log("record Updated successfully : " + ret.id);
-						   // ...
-						 });        
+  app.post(
+    "/api/createlead",
+    [
+      check("name").not().isEmpty().isLength({ min: 3, max: 255 }).trim(),
+      check("company").not().isEmpty().isLength({ min: 3, max: 255 }).trim(),
+    ],
+    postLimiter,
+    (request, response) => {
+      const errors = validationResult(request);
 
-				}else{
-					// Single record creation
-					conn.sobject("lead").create({         
-						Name : studentDtls[0].name,     
-						LastName: studentDtls[0].LastName,
-						Company: studentDtls[0].Company
-						}, function(err, ret) {
-						  if (err || !ret.success) { return console.error(err, ret); }
-						     console.log("Created record id : " + ret.id);
-						   // ...
-						 });    	
-				}  
+      if (!errors.isEmpty()) {
+        return response.status(422).json({ errors: errors.array() });
+      }
 
-			});
-		});
-			
-	    
-			
-      /*	
-      For Upsert
-      conn.sobject("Student__c").upsert({ 
-					Name : studentDtls[0].name,     
-					DateTaken__c: studentDtls[0].datetaken,
-					ExamResult__c: studentDtls[0].examresult,
-					MinutesTaken__c: studentDtls[0].minutestaken,
-					Student_Id__c: sId
-				}, 'Student_Id__c', function(err, ret) {
-				  if (err || !ret.success) { return console.error(err, ret); }
-				  console.log('Upserted Successfully');
-				  // ... 
-				}); */
-	    return res.json(data);
+      const { name, company } = request.body;
 
-	    })
-	    .catch(function (err) {
-	        console.log("ERROR:", err); // print the error;
-	        return res.status(500).json({ success: false,error : err});
-	    })
-	    .finally(function (){
-	        // If we do not close the connection pool when exiting the application,
-	        // it may take 30 seconds (poolIdleTimeout) before the process terminates,
-	        // waiting for the connection to expire in the pool.
-	
-	        // But if you normally just kill the process, then it doesn't matter.
-	        pgp.end(); // for immediate app exit, closing the connection pool.
-	        // See also:
-	        // https://github.com/vitaly-t/pg-promise#library-de-initialization
-	    });
-  	});
-   
-	
-	
+      pool.query(
+        "INSERT INTO lead (name, company) VALUES ($1, $2)",
+        [name, company],
+        (error) => {
+          if (error) {
+            throw error;
+          }
+          response
+            .status(201)
+            .json({ status: "success", message: "Lead Created." });
+        }
+      );
+    }
+  );
+
+  app.get("/", function (req, res) {
+    res.render("index.ejs"); // load the index.ejs file
+  });
+
+  app.get("/api/studentid/:sId", function (req, res) {
+    var sId = req.params.sId;
+    console.log("sId+" + sId);
+    //WHERE s_id ="+sId+"::int"
+    db.query("SELECT * FROM student where s_id=" + sId + "", true)
+      .then(function (data) {
+        console.log("data+" + data);
+        var studentDtls = data;
+
+        conn.login(process.env.SF_Username, process.env.SF_PWD, function (
+          err,
+          userInfo
+        ) {
+          if (err) {
+            return res.redirect("/orders");
+          }
+          // Now you can get the access token and instance URL information.
+          var records = [];
+          conn.query(
+            "SELECT DateTaken__c,ExamResult__c,MinutesTaken__c,Id,Name FROM Student__c WHERE Student_Id__c='" +
+              sId +
+              "' LIMIT 1",
+            function (err, result) {
+              if (err) {
+                return res.status(500).json({ success: false, error: err });
+              }
+              console.log("total : " + result.totalSize);
+              console.log("fetched : ", result.records[0]);
+              console.log("done ? : " + result.done);
+
+              records = result.records[0];
+
+              if (result.totalSize != 0) {
+                // record updation
+                conn.sobject("Student__c").update(
+                  {
+                    Id: result.records[0].Id,
+                    Name: result.records[0].name,
+                    DateTaken__c: result.records[0].datetaken__c,
+                    ExamResult__c: result.records[0].examresult__c,
+                    MinutesTaken__c: result.records[0].minutestaken__c,
+                  },
+                  function (err, ret) {
+                    if (err || !ret.success) {
+                      return console.error(err, ret);
+                    }
+                    console.log("record Updated successfully : " + ret.id);
+                    // ...
+                  }
+                );
+              } else {
+                // Single record creation
+                conn.sobject("Student__c").create(
+                  {
+                    Name: studentDtls[0].name,
+                    DateTaken__c: studentDtls[0].datetaken,
+                    ExamResult__c: studentDtls[0].examresult,
+                    MinutesTaken__c: studentDtls[0].minutestaken,
+                  },
+                  function (err, ret) {
+                    if (err || !ret.success) {
+                      return console.error(err, ret);
+                    }
+                    console.log("Created record id : " + ret.id);
+                    // ...
+                  }
+                );
+              }
+            }
+          );
+        });
+        return res.json(data);
+      })
+      .catch(function (err) {
+        console.log("ERROR:", err); // print the error;
+        return res.status(500).json({ success: false, error: err });
+      })
+      .finally(function () {
+        pgp.end(); // for immediate app exit, closing the connection pool.
+      });
+  });
 };
