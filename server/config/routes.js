8560705b841c7a36fc53858a056d176e79dcd125
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-04-22 15:58:44 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -16,22 +16,7 @@ module.exports = function (app, db, pgp) {
   // =====================================
   // HOME PAGE (with login links) ========
   // =====================================
- app.get("/api/getlead", function (req, res) {
-    //res.render("index.ejs"); // load the index.ejs file
-  const results = [];
-    var query = "SELECT * FROM lead ";
-    db.query(query, true)
-      .then(function (data) {
-        return res.json(data);
-      })
-      .catch(function (err) {
-        console.log("ERROR:", err); // print the error;
-        return res.status(400).json({ success: false, error: err });
-      })
-   .finally(function () {
-        pgp.end(); // for immediate app exit, closing the connection pool.
-      });
-  });
+
 
   app.post("/api/createlead", function (req, res) {
     const { name, company } = req.body;
